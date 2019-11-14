aee2adccafe035e5135249381e4ba81bb904d167
Draylon <draylid@gmail.com>
2019-11-14 09:45:10 -0300

    ruim

--- projeto_py/lib/parafusadeira.py
+++ projeto_py/lib/parafusadeira.py
@@ -1,11 +1,18 @@
 class Parafusadeira:
     def __init__(self):
-        self.movimento = False
-    
+        self.estoque = []
+        self.automato = Automato()
+        self.shut = False
 
-    def processo(self,fita):
-        for a in fita:
-            print(a)
+    def mainthread(self,blop,obj_list):
+        #reler fita
+        #pegar self.automato e avaliar fita
+        while not self.shut:
+            if len(obj_list[0].fita_list) > self.ultimaFita_len:
+                print("parafusadeira reading updates")
+                self.ultimaFita_len = len(obj_list[0].fita_list)
+        print("FML")
+        time.sleep(3)
 
             
 
